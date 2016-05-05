7437bebd01b995a924652a4b1d974f9a25459cb9
Ned M <cobracode@users.noreply.github.com>
2016-05-05 07:18:39 -0600

    Adding more to DeviceInfo activity; adding vcs.xml

--- app/src/main/java/ned/playground/DeviceInfo.java
+++ app/src/main/java/ned/playground/DeviceInfo.java
@@ -1,16 +1,47 @@
 package ned.playground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class DeviceInfo extends AppCompatActivity {
-    private static final String TAG = "DeviceInfo";
+    private static final String TAG = "DeviceInfo Activity";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_info);
-        Log.i(TAG, "constructed");
+        Log.i(TAG, "Created");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Restarted");
+    }
+
+    /**
+     * Dispatch onStart() to all fragments.  Ensure any created loaders are
+     * now started.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Started");
+    }
+
+    /**
+     * Dispatch onResume() to fragments.  Note that for better inter-operation
+     * with older versions of the platform, at the point of this call the
+     * fragments attached to the activity are <em>not</em> resumed.  This means
+     * that in some cases the previous state may still be saved, not allowing
+     * fragment transactions that modify the state.  To correctly interact
+     * with fragments in their proper state, you should instead override
+     * {@link #onResumeFragments()}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Resuming");
     }
 }
